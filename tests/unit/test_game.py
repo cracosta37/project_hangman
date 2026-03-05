3b6df1a3ca3b0fc15275f2600baf2b870e8b5560
cracosta37 <carlosroberto.acosta@gmail.com>
2026-03-05 15:19:04 -0500

    Added the Test Group 1 - Initialization

--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -23,3 +23,13 @@ def prepared_game(game_with_players):
     game_with_players.set_word("HELLO")
     return game_with_players
 
+def test_game_initializes_remaining_letters(game):
+    assert len(game.remaining_letters) == 26
+    assert "A" in game.remaining_letters
+    assert "Z" in game.remaining_letters
+
+def test_extended_alphabet_when_normalization_disabled(constants):
+    game = Game(constants, normalize_input=False)
+
+    assert "Ñ" in game.remaining_letters
+    assert "Á" in game.remaining_letters
